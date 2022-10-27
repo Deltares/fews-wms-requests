5cc99cf602eb14f5e1634587ba8558e062baaba7
Cees Voesenek <cees.voesenek@vortech.nl>
2022-10-27 09:59:09 +0200

    Expand and document GetLegendGraphic filter type

--- src/requestParameters/getLegendGraphicFilter.ts
+++ src/requestParameters/getLegendGraphicFilter.ts
@@ -1,5 +1,29 @@
 import {BaseWMSFilter} from "../requestParameters/baseWmsFilter";
 
 export interface GetLegendGraphicFilter extends BaseWMSFilter {
-    layers?: string
+    /** (optional, since 2020.02): The format of the GetLegendGraphic response.
+     *  Possible values are:  image/png (the default) or application/json. */
+    format?: "image/png" | "application/json"
+
+    /** The gridPlotId of the gridPlot which legend should be displayed. Only one
+     *  gridPlotId is supported. */
+    layers: string;
+
+    /** (optional): width of the legend. Default is 150. */
+    width?: number;
+
+    /** (optional): height of the image. The default is based on the number of
+     *  legend items, 15 pixels per item. */
+    height?: number;
+
+    /** (optional, since 2020.02): rescale the colors in the map by changing the
+     *  min and max values of the range. The minimuma value and maximum value are
+     *  separated by a , For example: colorscalerange=2.0,10.0. This parameter can
+     *  be used for the GetMap as well. */
+    colorscalerange?: string;
+
+    /** (optional, since 2020.02 ): a style can be passed as request parameter.
+     *  For example: style=Class.Temperature.Extreme. Available styles are
+     *  reported by the GetCapabilities request per layer. */
+    style?: string;
 }
