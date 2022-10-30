568ddaa723f59677a33d8012ada7834aed246568
Rudie Ekkelenkamp <ekkelenkamp@gmail.com>
2022-10-30 10:30:36 +0100

    junit report result folder.

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,12 @@ module.exports = {
     },
     testRegex: "\.spec\.(ts|tsx|js)$",
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    reporters: [ "default", "jest-junit" ],
+    reporters: [ "default", ["jest-junit",{
+        outputDirectory: 'reports', outputName: 'junit.xml',
+    } ] ],
     "testResultsProcessor": "jest-teamcity-reporter",
     setupFiles: ["./.env.test"],
-    "coverageReporters": ["lcov", "text", "teamcity"]
+    "coverageReporters": ["lcov", "text", "teamcity"],
+    testTimeout: 30 * 1000,
 }
+
